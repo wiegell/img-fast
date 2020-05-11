29a21f37b6ca3ccba2fb42018acb750a464a5d83
BWiegell <bj.wiegell@gmail.com>
2020-05-11 22:23:10 +0200

    wut

--- src/lib/img-fast.ts
+++ src/lib/img-fast.ts
@@ -1,5 +1,4 @@
-import { ExifParserFactory, ExifData } from "ts-exif-parser";
-import { ExifParser } from "ts-exif-parser/lib/ExifParser";
+
 import {
   globalConfigType,
   globalDefaultConfig,
@@ -12,32 +11,21 @@ import { Loader } from "./Loader";
 import { Subscription } from "rxjs";
 import { globalContainer } from "./stateKeeper";
 import { verboseLog } from "./helpers";
+import { main } from "./dataHandler";
 const css = "<style>" + require("../styles/main.css") + "</style>";
 const template: string = require("../templates/component.html");
 
 export class ImgFast extends HTMLElement {
   // private parser: ExifParser | undefined;
-  private data: ExifData | undefined;
-  private parser: ExifParser | undefined;
+  
   private glob: globalContainer = window.imgFastGlobalContainer;
   private loaderId: number = this.glob.getUniqueID();
-  // private status: elementStatusType = {
-  //   id: this.glob.getUniqueID(),
-  //   dlStatus: dlStatusEnum.Stopped,
-  //   isInViewport: false,
-  //   hasJustEnteredViewport: false,
-  // };
   private downloadSubscription: Subscription;
 
   constructor() {
     super();
     // Attach a shadow root to the element.
     let shadowRoot = this.attachShadow({ mode: "open" });
-
-    // Subscribe to global subject
-    // this.obs.areSVGsRendered.subscribe((input) => {
-    //   console.log("yey! ");
-    // });
 
     //Subscriptions
     this.downloadSubscription = this.glob.$dlDictator.subscribe((input) => {
@@ -50,32 +38,8 @@ export class ImgFast extends HTMLElement {
           this.src + "?" + Math.round(Math.random() * 100)
         );
         //Process onload
-        loader.getHTTPonLoadPromise().then((result: ArrayBuffer) => {
-          //Do parsing
-          this.parser = ExifParserFactory.create(result);
-          this.data = this.parser.parse();
-          if (this.data.hasThumbnail("image/jpeg")) {
-            // console.log(srcArray[i].substr(srcArray[i].length - 10));
-            // console.log(result);
-
-            //Make blob from thumbnail
-            let thumbnailArBu = this.data.getThumbnailBuffer();
-            let blob = new Blob([thumbnailArBu], { type: "image/jpeg" });
-            let urlCreator = window.URL || window.webkitURL;
-            let imageUrl = urlCreator.createObjectURL(blob);
-
-            //Append thumbnail
-            let img = document.createElement("img");
-            img.src = imageUrl;
-            let tmp: HTMLElement | null = shadowRoot.getElementById(
-              "ratio_keeper"
-            );
-            if (tmp != null) {
-              tmp.appendChild(img);
-            } else {
-              console.error();
-            }
-          }
+        loader.getHTTPonLoadPromise().then( result => {
+          main(result, this.shadowRoot)
         });
 
         //Send HTTP
