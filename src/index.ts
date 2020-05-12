e7bf17f64e1ff549ded6e4cd491ec1b06fa9de59
BWiegell <bj.wiegell@gmail.com>
2020-05-12 21:02:32 +0200

    npm test

--- src/index.ts
+++ src/index.ts
@@ -2,23 +2,25 @@
 // import { EventBus } from "ts-bus";
 import { ImgFast } from "./lib/img-fast";
 import { globalContainer } from "./lib/stateKeeper";
-
 declare global {
   interface Window {
     imgFastGlobalContainer: globalContainer;
   }
 }
 
-if (window.imgFastGlobalContainer !== undefined) {
-  if (window.imgFastGlobalContainer.config == undefined) {
-    console.warn("imgFastGlobal already defined, but no config is set");
+export function globalInit() {
+  if (window.imgFastGlobalContainer !== undefined) {
+    if (window.imgFastGlobalContainer.config == undefined) {
+      console.warn("imgFastGlobal already defined, but no config is set");
+    } else {
+      console.log("Global config is set (TODO: typeguard this config)");
+    }
   } else {
-    console.log("Global config is set (TODO: typeguard this config)");
+    console.log("Defining global behaviorSubject");
+    window.imgFastGlobalContainer = new globalContainer();
   }
-} else {
-  console.log("Defining global behaviorSubject");
-  window.imgFastGlobalContainer = new globalContainer();
 }
 
+globalInit();
 // export ImgFast?
 customElements.define("img-fast", ImgFast);
